fabdc1e4bfbfa13ee8736288e95a4334410611e2
Ryan Bigg <git@ryanbigg.com>
2015-07-31 14:41:28 +1000

    Add Lightspeed::Base#attributes
    Used to get the raw attributes that are passed to Lightspeed::Base#initialize

--- lib/lightspeed/base.rb
+++ lib/lightspeed/base.rb
@@ -1,9 +1,10 @@
 module Lightspeed
   class Base
-    attr_accessor :id, :owner
+    attr_accessor :id, :owner, :attributes
 
     def initialize(owner, data = {})
       @owner = owner
+      @attributes = data
       self.id = data.delete(self.class.id_field)
       data.each do |k, v|
         send("#{k}=", v)
@@ -12,6 +13,10 @@ module Lightspeed
 
     def inspect
       "#<#{self.class.name} id=#{id}>"
+    end
+
+    def to_json
+      attributes.to_json
     end
   end
 end
--- spec/lightspeed/item_spec.rb
+++ spec/lightspeed/item_spec.rb
@@ -14,4 +14,12 @@ describe Lightspeed::Item do
       expect(matrix).to be_a(Lightspeed::ItemMatrix)
     end
   end
+
+  it "can get the attributes of an item" do
+    expect(subject.attributes).to eq({ id: 2 })
+  end
+
+  it "can show a JSON representation of an item" do
+    expect(subject.to_json).to eq({ id: 2}.to_json)
+  end
 end
