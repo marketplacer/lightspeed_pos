aff9cbcbc1f6d5e9ec5aaa99dd117a9e69c7c8fd
Ryan Bigg <git@ryanbigg.com>
2015-07-31 15:53:58 +1000

    Rubocop on items_spec

--- spec/lightspeed/item_spec.rb
+++ spec/lightspeed/item_spec.rb
@@ -22,10 +22,10 @@ describe Lightspeed::Item do
   end
 
   it "can get the attributes of an item" do
-    expect(subject.attributes).to eq({ id: 2 })
+    expect(subject.attributes).to eq(id: 2)
   end
 
   it "can show a JSON representation of an item" do
-    expect(subject.to_json).to eq({ id: 2}.to_json)
+    expect(subject.to_json).to eq({ id: 2 }.to_json)
   end
 end
