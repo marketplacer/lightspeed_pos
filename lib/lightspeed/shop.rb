0f6d230de61ea42683ff3563f71c454f1d3978a5
Ana Djordjevic <djordjana@gmail.com>
2015-09-25 12:18:56 +1000

    only need a name

--- lib/lightspeed/shop.rb
+++ lib/lightspeed/shop.rb
@@ -3,18 +3,8 @@ require_relative 'resource'
 module Lightspeed
   class Shop < Lightspeed::Resource
     alias_method :archive, :destroy
-
-    # attr_accessor :systemSku, :defaultCost, :avgCost, :discountable, :tax,
-    #               :archived, :itemType, :description, :modelYear, :upc, :ean, :customSku,
-    #               :manufacturerSku, :createTime, :timeStamp,
-    # 
-    #               # Association keys
-    #               :taxClassID, :departmentID, :manufacturerID, :seasonID,
-    #               :defaultVendorID, :itemECommerceID,
-    # 
-    #               # Embedded
-    #               :ItemAttributes, :ItemShops, :Prices, :Note, :TaxClass,
-    #               :Manufacturer, :ItemVendorNums, :CustomFieldValues, :Tags
+    
+    attr_accessor :name
 
     relate :Items
   end
