ecd46846c6f69d157606e765ef470c5c844fd649
Jordan Sitkin <jordan@fiftyfootfoghorn.com>
2015-11-03 23:45:28 -0800

    VCR requests shouldn't consider Lightspeed account ID

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,11 +8,24 @@ Dir[File.expand_path('support/**/*.rb', File.dirname(__FILE__))].each do |f|
   require f
 end
 
-Lightspeed::TEST_OAUTH_TOKEN = ENV.fetch('LIGHTSPEED_OAUTH_TOKEN', 'test')
+Lightspeed::TEST_OAUTH_TOKEN = ENV.fetch('LIGHTSPEED_OAUTH_TOKEN', '')
+Lightspeed::TEST_API_KEY = ENV.fetch('LIGHTSPEED_API_KEY', '')
 Lightspeed::TEST_ACCOUNT_ID = ENV.fetch('LIGHTSPEED_ACCOUNT_ID', '117102').to_i
 
 VCR.configure do |config|
+  config.register_request_matcher :api_endpoint do |*reqs|
+    call_1, call_2 = reqs.map do |req|
+      # if the path includes an account number, only use path fragment after it
+      # if the path does not include an account number, just use URI
+      URI(req.uri).path.scan(/^\/.*\/\d+\/(.*)$/).flatten.first || req.uri
+    end
+    call_1 == call_2
+  end
+
   config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
   config.hook_into :webmock
-  config.default_cassette_options = { record: :new_episodes }
+  config.default_cassette_options = {
+    record: :new_episodes,
+    match_requests_on: [:api_endpoint, :method, :body]
+  }
 end
