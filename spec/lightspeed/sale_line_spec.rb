88e72be214b234ddc9de77973d75ea2f0b05490d
Jordan Sitkin <jordan@fiftyfootfoghorn.com>
2015-11-16 14:45:53 -0800

    sale line specs

--- spec/lightspeed/sale_line_spec.rb
+++ spec/lightspeed/sale_line_spec.rb
@@ -3,8 +3,22 @@ require 'spec_helper'
 describe Lightspeed::SaleLine do
   setup_client_and_account
 
-  it "can fetch an associated item"
-  it "can show a JSON representation of a sale line"
+  subject do
+    Lightspeed::SaleLine.new(context: account, attributes: {
+      "saleLineID" => 4,
+      "itemID" => 177
+    })
+  end
+
+  it "can fetch an associated item" do
+    VCR.use_cassette("account/items/show") do
+      expect(subject.item).to be_a(Lightspeed::Item)
+    end
+  end
+
+  it "can get the attributes of a sale line" do
+    expect(subject.attributes).to eq("saleLineID" => 4, "itemID" => 177)
+  end
 
 end
 
