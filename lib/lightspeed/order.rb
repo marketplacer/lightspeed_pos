5f7cd228e6ca53bd061511ac87f9dea8dcd65a69
Jordan Sitkin <jordan@fiftyfootfoghorn.com>
2015-11-23 11:02:38 -0800

    basic orders

---
+++ lib/lightspeed/order.rb
@@ -0,0 +1,34 @@
+require_relative 'resource'
+
+module Lightspeed
+  class Order < Lightspeed::Resource
+    alias_method :archive, :destroy
+
+    fields(
+      orderID: :id,
+      orderedDate: :timestamp,
+      receivedDate: :timestamp,
+      arrivalDate: :timestamp,
+      shipInstructions: :integer,
+      stockInstructions: :integer,
+      shipCost: :decimal,
+      otherCost: :decimal,
+      complete: :boolean,
+      archived: :boolean,
+      discount: :boolean,
+      totalDiscount: :decimal,
+      totalQuantity: :decimal,
+      vendorID: :id,
+      noteID: :id,
+      shopID: :id,
+      Vendor: :hash,
+      Note: :hash,
+      Shop: :hash,
+      OrderLines: :hash,
+      CustomFieldValues: :hash,
+      timeStamp: :timestamp
+    )
+
+  end
+end
+
---
+++ lib/lightspeed/orders.rb
@@ -0,0 +1,10 @@
+require_relative 'collection'
+
+require_relative 'order'
+
+module Lightspeed
+  class Orders < Lightspeed::Collection
+    alias_method :archive, :destroy
+  end
+end
+
