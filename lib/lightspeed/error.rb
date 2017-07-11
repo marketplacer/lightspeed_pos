ed1ab34d9fb276fd29087767efeb376fb7bb8b33
Nigel Williams <nigelw@marketplacer.com>
2017-07-11 10:39:58 +1000

    Have a separate exception class for 403 errors (logged in and denied access)

--- lib/lightspeed/error.rb
+++ lib/lightspeed/error.rb
@@ -2,6 +2,7 @@ module Lightspeed
   class Error < Exception
     class BadRequest < Exception; end # 400
     class Unauthorized < Exception; end # 401
+    class NotAuthorized < Exception; end # 403
     class NotFound < Exception; end # 404
     class InternalServerError < Exception; end # 500
     class Throttled < Exception; end # 503
