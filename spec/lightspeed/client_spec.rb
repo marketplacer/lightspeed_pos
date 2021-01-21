54e73f4843f35d927b8beeb7e7904f70f56c284c
Ricky Manriquez <dechimp@gmail.com>
2021-01-21 15:07:42 -0700

    use double splat for keyword argument expansion

--- spec/lightspeed/client_spec.rb
+++ spec/lightspeed/client_spec.rb
@@ -2,21 +2,6 @@
 
 require 'spec_helper'
 
-class ExampleTokenHolder
-  def initialize(token: nil, refresh_token: nil)
-    @token = token
-    @refresh_token = refresh_token
-  end
-
-  def oauth_token
-    @token
-  end
-
-  def refresh_oauth_token
-    @token = "#{@token}_refreshed"
-  end
-end
-
 describe Lightspeed::Client do
   it "can set an oauth token holder and fetch an access token from it" do
     key = 'test'
